5decba003254d95af646f5d75100cce896f90e58
zeditya <adityabahuguna098@gmail.com>
2026-02-11 12:00:00 +0530

    DSA Day 11

---
+++ Day11/Day11.c
@@ -0,0 +1,32 @@
+// Day 11 of #100daysofDSA
+
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int a[m][n], b[m][n], sum[m][n];
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

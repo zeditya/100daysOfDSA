31ee72b32ba412478fe74876278ff0af4ce34430
zeditya <adityabahuguna098@gmail.com>
2026-02-17 12:00:00 +0530

    DSA Day 17

---
+++ Day17/Day17.c
@@ -0,0 +1,30 @@
+// Day 17 of #100daysofDSA
+
+ #include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) return 0;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("Max: %d\n", max);
+    printf("Min: %d\n", min);
+
+    return 0;
+}

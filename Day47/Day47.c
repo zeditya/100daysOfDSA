d4ecb96b1f0ced25968c7b414b061b9c1b809b10
zeditya <adityabahuguna098@gmail.com>
2026-03-19 12:00:00 +0530

    DSA Day 47

---
+++ Day47/Day47.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Height of Binary Tree implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}

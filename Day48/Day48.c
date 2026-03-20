1737ceeb95af0d78add63e4ebb94bbfd177ad80a
zeditya <adityabahuguna098@gmail.com>
2026-03-20 12:00:00 +0530

    DSA Day 48

---
+++ Day48/Day48.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Count Leaf Nodes implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}

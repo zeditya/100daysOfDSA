9f79437019726cfeb55cc4e6b5f2de071ae258ee
zeditya <adityabahuguna098@gmail.com>
2026-04-25 12:00:00 +0530

    DSA Day 84

---
+++ Day84/Day84.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    for(int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    insertionSort(arr, n);
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
